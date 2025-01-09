7b9cc1e6a32aef3a8effb7acd55a806782d59d95
Sasikumar Sahkana <shakana2604@gmail.com>
2025-01-09 08:57:32 +0100

    Menu

--- main.cpp
+++ main.cpp
@@ -1,7 +1,62 @@
 #include <iostream>
+using namespace std;
+
+struct Account {
+    int accountNumber;
+    char firstName[20];
+    char lastName[20];
+    char phoneNumber[10];
+    float balance;
+};
+
+void addRecord() {
+}
+
+void showRecords() {
+
+}
+
+void searchRecord() {
+
+}
+
+void updateRecord() {
+
+}
+
+void deleteRecord() {
+
+}
 
 int main()
 {
-    std::cout << "PRojet C++" << std::endl;
+    bool show = true;
+
+    while (show) {
+        cout << "\n***Account Information System***" << endl;
+        cout << "Select one option below" << endl;
+        cout << "\t 1 --> Add record to file" << endl;
+        cout << "\t 2 --> Show record from file" << endl;
+        cout << "\t 3 --> Search record from file" << endl;
+        cout << "\t 4 --> Update record" << endl;
+        cout << "\t 5 --> Delete record" << endl;
+        cout << "\t 6 --> Quit" << endl;
+        cout << "Enter your choice :" << endl;
+
+        char choice;
+        cin >> choice;
+
+        switch (choice) {
+            case '1': addRecord(); break;
+            case '2': showRecords(); break;
+            case '3': searchRecord(); break;
+            case '4': updateRecord(); break;
+            case '5': deleteRecord(); break;
+            case '6': show = false; break;
+            default: cout << "Invalid choice, please try again." << endl;
+        }
+    }
+
+    cout << "Have a good day !" << endl;
     return 0;
 }
